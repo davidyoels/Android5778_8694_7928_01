7d7f8bd71e0a87b04fc1a7c909521974638a1974
itay0421 <itay0421@gmail.com>
2017-11-14 18:19:29 +0200

    tray

--- app/src/main/java/com/example/davidsalmon/android5778_8694_7928_01/model/backend/FactoryMethod.java
+++ app/src/main/java/com/example/davidsalmon/android5778_8694_7928_01/model/backend/FactoryMethod.java
@@ -3,11 +3,11 @@ package com.example.davidsalmon.android5778_8694_7928_01.model.backend;
 import com.example.davidsalmon.android5778_8694_7928_01.model.datasource.List_DBManager;
 
 /**
- * Created by david salmon on 11/3/2017.
+ * Created by david salm on on 11/3/2017.
  */
 
 public class FactoryMethod {
-    static DB_manager manager = null;
+    static DB_manager manager = null ;
 
     public static DB_manager getManager() {
         if (manager == null)
