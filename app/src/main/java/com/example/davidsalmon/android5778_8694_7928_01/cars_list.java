df17cf9814990c42cef0c8cd3bcbff555b08444c
david <davidyoels@gmail.com>
2017-11-22 11:46:20 +0200

    tray1eqwe

--- app/src/main/java/com/example/davidsalmon/android5778_8694_7928_01/cars_list.java
+++ app/src/main/java/com/example/davidsalmon/android5778_8694_7928_01/cars_list.java
@@ -1,15 +1,30 @@
 package com.example.davidsalmon.android5778_8694_7928_01;
 
 import android.app.Activity;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.solver.widgets.ConstraintAnchor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import com.example.davidsalmon.android5778_8694_7928_01.model.backend.FactoryMethod;
+import com.example.davidsalmon.android5778_8694_7928_01.model.entities.Client;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class cars_list extends Activity {
+
+    List myList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cars_list);
+
+
     }
 }
