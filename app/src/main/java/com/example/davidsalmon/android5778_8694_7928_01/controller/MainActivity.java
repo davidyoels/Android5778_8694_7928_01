60392833704fb03ba24091a449e81a738424f0a4
itay0421 <itay0421@gmail.com>
2017-11-26 16:44:47 +0200

    סיימתי את הlist של רכב

--- app/src/main/java/com/example/davidsalmon/android5778_8694_7928_01/controller/MainActivity.java
+++ app/src/main/java/com/example/davidsalmon/android5778_8694_7928_01/controller/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //create one mofa
+        //create one
         b = a.getManager();
         findViews();
     }
